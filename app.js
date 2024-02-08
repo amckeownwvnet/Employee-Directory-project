c6c4c15675736a1568b866a35e76a7f967de7042
amckeownwvnet <116903775+amckeownwvnet@users.noreply.github.com>
2024-02-08 13:51:23 -0500

    Fix street call issue.
    herp derp notate the sub info

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const modalHTML = `
 <p class="address">${city}</p>
 <hr />
 <p>${phone}</p>
-<p class="address"> ${city} ${street}, ${state} ${postcode}</p>
+<p class="address">${street.number} ${street.name}  ${city} , ${state} ${postcode}</p>
 <p>Birthday:
 ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
 </div>
